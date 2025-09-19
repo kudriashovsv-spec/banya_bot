406d5ea9045d964c26e1fa79a4665142d71efb4c
Sergey <kudirashovsv@gmail.com>
2025-09-19 10:33:48 +0300

    Implement TipsManager and fix imports

--- banya_bot.py
+++ banya_bot.py
@@ -8,6 +8,8 @@ from managers.quiz_manager import QuizManager
 from handlers.main_handlers import register_handlers, load_subscriptions, save_subscriptions
 from handlers.quiz_handlers import register_quiz_handlers
 from datetime import datetime, time, timedelta
+from managers.tips_manager import TipsManager
+
 
 
 # Загружаем токен
@@ -22,16 +24,18 @@ dp = Dispatcher()
 banya_mgr = BanyaManager()
 encyclopedia_mgr = EncyclopediaManager()
 quiz_mgr = QuizManager()
+tips_mgr = TipsManager()
+
 
 # Регистрируем хендлеры
 register_handlers(dp, banya_mgr, encyclopedia_mgr, quiz_mgr)
 register_quiz_handlers(dp, quiz_mgr)
 
 
-async def daily_advice_sender(bot, encyclopedia_mgr):
+async def daily_advice_sender(bot, tips_mgr):
     while True:
         now = datetime.now()
-        target_time = datetime.combine(now.date(), time(hour=17, minute=0))
+        target_time = datetime.combine(now.date(), time(hour=10, minute=40))
         if now > target_time:
             target_time += timedelta(days=1)
         wait_seconds = (target_time - now).total_seconds()
@@ -40,23 +44,23 @@ async def daily_advice_sender(bot, encyclopedia_mgr):
         subs = load_subscriptions()
         for user_id, info in subs.items():
             if info.get("enabled"):
-                advice = encyclopedia_mgr.get_random_advice()  # словарь {"text": ..., "photo": ...}
+                advice = tips_mgr.get_random_tip()
                 text = advice["text"]
 
                 # --- Проверка выходных ---
                 weekday = now.weekday()  # 0=понедельник, 6=воскресенье
-                if weekday in [4,5,6]:  # Пятница, Суббота, Воскресенье
+                if weekday in [4,5,6]:
                     text += "\n🔥 Сегодня отличный день для посещения бани!"
 
                 # --- Отправка фото или только текста ---
-                if advice.get("photo"):
-                    await bot.send_photo(int(user_id), photo=advice["photo"], caption=text)
+                if advice.get("image"):
+                    await bot.send_photo(int(user_id), photo=advice["image"], caption=text)
                 else:
                     await bot.send_message(int(user_id), f"💡 Совет дня:\n{text}")
 
 async def main():
     print("Бот запущен")
-    asyncio.create_task(daily_advice_sender(bot, encyclopedia_mgr))
+    asyncio.create_task(daily_advice_sender(bot, tips_mgr))
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
